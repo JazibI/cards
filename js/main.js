8f1cac031fd9cc564d8f4dd3c4e99c2213229e64
ShahidBinShafiq <shahidvital786@gmail.com>
2024-07-29 18:40:45 +0500

    task done

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 3,
+//       spaceBetween: 30,
+//       pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//       },
+//   cssMode: true,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+//   mousewheel: true,
+//   keyboard: true,
+// });
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  // centeredSlides: true,
+  // loop: true,
+  breakpoints: {
+    480: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2,
+        spaceBetween: 28
+    },
+    980: {
+        slidesPerView: 3,
+        spaceBetween: 28,
+        resistanceRatio: 0.85
+    }
+},
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
